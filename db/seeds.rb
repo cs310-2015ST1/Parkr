cc27645a247745c0ae873e8b1175d0e08e3f230c
Mac Sampson <macsampson99@gmail.com>
2015-07-14 17:22:33 -0700

    Move parsers into the models folder, add calls to them from seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-ParkingMetersHelper
-CrimeDataHelper
-
+CrimeParser
+ParkingMeterParser
 
 
 
