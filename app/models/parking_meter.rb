865d4b584d9a338a0a111a7ab3c25a2bafcf72e9
Claire <claire.you@aol.com>
2015-07-16 06:09:56 -0700

    commented out toggleEV function

--- app/models/parking_meter.rb
+++ app/models/parking_meter.rb
@@ -1,6 +1,6 @@
 class ParkingMeter < ActiveRecord::Base
   markable_as :favorite, :by => :user
-
+  attr_accessor :visited
   reverse_geocoded_by :lat, :lng
   #after_validation :reverse_geocode
 end
